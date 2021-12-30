fc5be99a94354b89ddfa5c46eaec09a3835bb93e
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-12-30 13:28:19 -0800

    ChoiceList choice submission + styles

--- src/components/ChoiceList.tsx
+++ src/components/ChoiceList.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import { idText } from "typescript";
 import { useChoices } from "../hooks/useChoices";
+import { IChoice } from "../models/IChoice";
 import { IFactor } from "../models/IFactor";
 import styles from "../styles/choicelist.module.css";
 
@@ -8,18 +10,55 @@ interface ChoiceListProps {
 }
 
 export const ChoiceList = ({ factors }: ChoiceListProps): JSX.Element => {
-  // const [choices] = useChoices(factors);
+  const [choices, addChoice] = useChoices(factors);
+  const [choiceInput, setChoiceInput] = useState<string>("");
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>): void => {
+    setChoiceInput(event.currentTarget.value);
+  };
+
+  const handleChoiceSubmit = (
+    event: React.KeyboardEvent<HTMLInputElement>
+  ): void => {
+    if (event.key === "Enter") {
+      const id =
+        choices.reduce(
+          (prev, current) => {
+            return prev.id > current.id ? prev : current;
+          },
+          { id: 0 }
+        ).id + 1;
+      const newChoice: IChoice = {
+        title: choiceInput.trim(),
+        id: id,
+        chosen: false,
+        factors: [],
+      };
+      addChoice(newChoice);
+      setChoiceInput("");
+    }
+  };
 
   return (
     <section id="choices" className={styles.Container}>
       <span>
         <h2 className={styles.Title}>Choices</h2>
       </span>
-      <input
-        type="text"
-        placeholder="Enter a new choice here"
-        className={styles.ChoiceInput}
-      />
+      <section id="choice-list" className={styles.Body}>
+        <ul>
+          {choices.map((choice, index) => {
+            return <li key={index}>{choice.title}</li>;
+          })}
+        </ul>
+        <input
+          type="text"
+          placeholder="Enter a new choice here"
+          className={styles.ChoiceInput}
+          value={choiceInput}
+          onChange={handleChange}
+          onKeyPress={handleChoiceSubmit}
+        />
+      </section>
     </section>
   );
 };
