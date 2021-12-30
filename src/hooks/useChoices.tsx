a5f20e066c1d28c7fb9db30198a9367565c4379e
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-12-30 13:28:00 -0800

    addChoice function created on useChoices hook

--- src/hooks/useChoices.tsx
+++ src/hooks/useChoices.tsx
@@ -2,8 +2,14 @@ import { useState } from "react";
 import { IChoice } from "../models/IChoice";
 import { IFactor } from "../models/IFactor";
 
-export const useChoices = (globalFactors: IFactor[]): [IChoice[]] => {
+export const useChoices = (
+  globalFactors: IFactor[]
+): [IChoice[], (choice: IChoice) => void] => {
   const [choices, setChoices] = useState<IChoice[]>([]);
 
-  return [choices];
+  const addChoice = (choice: IChoice): void => {
+    setChoices([...choices, choice]);
+  };
+
+  return [choices, addChoice];
 };
