61911b9f0062f2ad7681fa53f458cd96d3780fc9
maybemaby <74226064+maybemaby@users.noreply.github.com>
2021-12-31 10:53:53 -0800

    useChoices side effects for factors add/remove

--- src/hooks/useChoices.tsx
+++ src/hooks/useChoices.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { IChoice } from "../models/IChoice";
 import { IFactor } from "../models/IFactor";
 
@@ -7,8 +7,31 @@ export const useChoices = (
 ): [IChoice[], (choice: IChoice) => void] => {
   const [choices, setChoices] = useState<IChoice[]>([]);
 
-  const addChoice = (choice: IChoice): void => {
-    setChoices([...choices, choice]);
+  useEffect(() => {
+    if (globalFactors.length > choices[0].factors.length) {
+      const newChoices = choices.map((choice: IChoice): IChoice => {
+        const newFactors = choice.factors.slice();
+        newFactors.push({ ...globalFactors[-1] });
+        return { ...choice, factors: newFactors };
+      });
+      setChoices(newChoices);
+    } else if (globalFactors.length < choices[0].factors.length) {
+      const factorTitles = globalFactors.map(
+        (factor: IFactor): string => factor.title
+      );
+      const newChoices = choices.map((choice: IChoice): IChoice => {
+        const newFactors = choice.factors.filter((factor: IFactor): boolean => {
+          return factorTitles.includes(factor.title);
+        });
+        return { ...choice, factors: newFactors };
+      });
+      setChoices(newChoices);
+    }
+  }, [globalFactors]);
+
+  const addChoice = (choice: { title: string; id: number }): void => {
+    const newChoice = { ...choice, chosen: false, factors: [...globalFactors] };
+    setChoices([...choices, newChoice]);
   };
 
   return [choices, addChoice];
